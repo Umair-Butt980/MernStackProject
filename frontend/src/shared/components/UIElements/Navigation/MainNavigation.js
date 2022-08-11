a51feb17ebbd782c873f77ba120e849fc293942d
UmairButt <umairamjadbutt1@gmail.com>
2022-08-11 23:40:24 +0500

    Main Navigation and header created

---
+++ frontend/src/shared/components/UIElements/Navigation/MainNavigation.js
@@ -0,0 +1,17 @@
+import React from "react";
+import MainHeader from "./MainHeader";
+
+import "./MainNavigation.css"
+
+const MainNavigation = props => {
+    return 
+    <MainHeader>
+        <button>
+            <span/>
+            <span/>
+            <span/>
+        </button>
+    </MainHeader>
+}
+
+export default MainNavigation;
