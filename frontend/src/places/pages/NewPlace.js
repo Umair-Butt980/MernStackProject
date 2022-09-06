c1755b0bb833bccef375738785e8fa199f88ceb9
UmairButt <umairamjadbutt1@gmail.com>
2022-09-06 10:46:57 +0500

    adding the css for new place and adding the dynamic inout field

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,10 +1,11 @@
 import React from "react";
 
+import Input from "../../shared/components/FormElements/Input";
 import "./NewPlace.css";
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input type="text" label-="title" validators="check" />
+      <Input element="input" type="text" label="title" validators="check" />
     </form>
   );
 };
