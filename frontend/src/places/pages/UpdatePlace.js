34d79cd082c3fa5aaaef4cf6057d7df33a1ed578
UmairButt <umairamjadbutt1@gmail.com>
2022-09-11 14:52:51 +0500

    adjusting the input components

--- frontend/src/places/pages/UpdatePlace.js
+++ frontend/src/places/pages/UpdatePlace.js
@@ -1,77 +1,77 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
-import "./UpdatePlace.css";
-import Input from "../../shared/components/FormElements/Input";
-import Button from "../../shared/components/FormElements/Button";
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
 import {
   VALIDATOR_REQUIRE,
-  VALIDATOR_MINLENGTH,
-} from "../../shared/util/validators";
+  VALIDATOR_MINLENGTH
+} from '../../shared/util/validators';
+import './PlaceForm.css';
 
 const DUMMY_PLACES = [
   {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
     imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "20 W 34th St, New York, NY 10001",
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
     location: {
       lat: 40.7484405,
-      lng: -73.9878584,
+      lng: -73.9878584
     },
-    creator: "u1",
+    creator: 'u1'
   },
   {
-    id: "p2",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
+    id: 'p2',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
     imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "20 W 34th St, New York, NY 10001",
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
     location: {
       lat: 40.7484405,
-      lng: -73.9878584,
+      lng: -73.9878584
     },
-    creator: "u2",
-  },
+    creator: 'u2'
+  }
 ];
 
 const UpdatePlace = () => {
   const placeId = useParams().placeId;
 
-  let identifiedPlace = DUMMY_PLACES.find((place) => place.id === placeId);
+  const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
+
   if (!identifiedPlace) {
     return (
       <div className="center">
-        <h1> Could not find the desired Place</h1>
+        <h2>Could not find place!</h2>
       </div>
     );
   }
+
   return (
-    <form>
+    <form className="place-form">
       <Input
         id="title"
         element="input"
         type="text"
         label="Title"
-        validator={[VALIDATOR_REQUIRE()]}
-        errorText="Please Enter a valid title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
         onInput={() => {}}
-        valud={identifiedPlace.title}
+        value={identifiedPlace.title}
         valid={true}
       />
-      {/* Description Input */}
       <Input
         id="description"
-        element="input"
-        type="text"
+        element="textarea"
         label="Description"
-        validator={[VALIDATOR_MINLENGTH(5)]}
-        errorText="Please Enter a valid Description of minimum 5 letters"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (min. 5 characters)."
         onInput={() => {}}
-        valud={identifiedPlace.description}
+        value={identifiedPlace.description}
         valid={true}
       />
       <Button type="submit" disabled={true}>
